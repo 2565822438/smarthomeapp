6a021e0a9dffaaab1aca56d047bdb5d4841fcb16
qing <qing101999@163.com>
2022-03-28 21:27:53 +0800

    收发消息

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,6 +7,11 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    protected $commands =[
+        \App\Console\Commands\TestMqtt::class,
+        \App\Console\Commands\Subscribe::class,
+     
+    ];
     /**
      * Define the application's command schedule.
      *
